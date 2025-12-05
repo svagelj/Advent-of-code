2edfbb81955a1685ce2c6e815da930fdc3d8e1cf
svagelj <vito.svagelj@gmail.com>
2025-12-05 14:01:05 +0100

    adds 2025 day 4 - part 2

--- 2025/day_04/day_04.go
+++ 2025/day_04/day_04.go
@@ -31,7 +31,7 @@ var testData = []string {
 	"@.@.@@@.@.",
 }
 
-var testSolution1, testSolution2 = 13, -1
+var testSolution1, testSolution2 = 13, 43
 
 //------------------------------------------------------
 
@@ -67,6 +67,38 @@ func initData(fileLines []string, rollChar rune) ([][]rune, [][]int) {
 	return rollsMap, rollsPositions
 }
 
+func canRollBeRemoved(rollsMap [][]rune, rollPosition []int, yMax int, xMax int, rollChar rune) bool {
+
+	y := rollPosition[0]
+	x := rollPosition[1]
+	nFound := 0
+
+	// loops to get all combinations of x=+-1, y=+-1
+	for i := -1; i <= 1; i++ {
+		for j := -1; j <= 1; j++ {
+			if i == 0 && j == 0 {
+				continue
+			}
+			if y + i < 0 || y + i >= yMax {
+				continue
+			}
+			if x + j < 0 || x + j >= xMax {
+				continue
+			}
+			
+			if rollsMap[y+i][x+j] == rollChar {
+				nFound = nFound + 1
+			}
+		}
+	}
+
+	if nFound <= 3 {
+		return true
+	} else {
+		return false
+	}
+}
+
 func solve1(rollsMap [][]rune, rollsPositions [][]int, rollChar rune, printout bool) int {
 
 	if printout {
@@ -81,31 +113,11 @@ func solve1(rollsMap [][]rune, rollsPositions [][]int, rollChar rune, printout b
 	sum := 0
 	for k := range rollsPositions {
 
-		pos := rollsPositions[k]
-		y := pos[0]
-		x := pos[1]
-		nFound := 0
+		pos := rollsPositions[k]		
 
-		// loops to get all combinations of x=+-1, y=+-1
-		for i := -1; i <= 1; i++ {
-			for j := -1; j <= 1; j++ {
-				if i == 0 && j == 0 {
-					continue
-				}
-				if y + i < 0 || y + i >= yMax {
-					continue
-				}
-				if x + j < 0 || x + j >= xMax {
-					continue
-				}
-				
-				if rollsMap[y+i][x+j] == rollChar {
-					nFound = nFound + 1
-				}
-			}
-		}
-
-		if nFound <= 3{
+		if canRollBeRemoved(rollsMap, pos, yMax,xMax, rollChar) {
+			y := pos[0]
+			x := pos[1]
 			found[y][x] = rune('x')
 			sum = sum + 1
 		}
@@ -119,14 +131,61 @@ func solve1(rollsMap [][]rune, rollsPositions [][]int, rollChar rune, printout b
 }
 
 //----------------------------------------
-
-func solve2(batteryBanks []string, printout bool) int {
+func solve2(rollsMap [][]rune, rollsPositions [][]int, rollChar rune, printout bool) int {
 
 	if printout {
-		fmt.Println("battery bank:", batteryBanks)
+		Printer.PrintGridRune(rollsMap, 1)
+		fmt.Println(rollsPositions)
 	}
 
+	yMax := len(rollsMap)
+	xMax := len(rollsMap[0])
+	
+	currentState := Array.CopyRune2D(rollsMap)
+	currentRolls := make([][]int, len(rollsPositions))
+	copy(currentRolls, rollsPositions)
+
 	sum := 0
+	maxIter := 999
+	for range maxIter {
+		nRemoved := 0
+		rollsToRemove := [][]int {}
+		newRolls := [][]int {}
+
+		// Find possible rolls to remove
+		for i := range currentRolls {
+			pos := currentRolls[i]		
+
+			if canRollBeRemoved(currentState, pos, yMax,xMax, rollChar) {
+				rollsToRemove = append(rollsToRemove, pos)
+			} else {
+				newRolls = append(newRolls, pos)
+			}
+		}
+
+		if len(rollsToRemove) == 0 {
+			break
+		}
+
+		// Remove rolls
+		for i := range rollsToRemove {
+			y := rollsToRemove[i][0]
+			x := rollsToRemove[i][1]
+			currentState[y][x] = rune('.')
+			nRemoved = nRemoved + 1
+		}
+
+		if printout {
+			fmt.Println("--------------")
+			fmt.Println("removed", nRemoved)
+			fmt.Println("to remove", rollsToRemove)
+			Printer.PrintGridRune(currentState, 1)
+		}
+		
+		currentRolls = newRolls
+		sum = sum + nRemoved
+	}
+
 	return sum
 }
 
@@ -151,13 +210,13 @@ func main() {
 	fmt.Println("Solution part 1 =", sol1, "(ET =", dur, ")")
 
 	// ---------------------------------------------
-	// fmt.Println()
-	// fmt.Println("=== Part 2 ===")
-	// sol2_1_test := solve2(batteryBanksTest, true)
-	// fmt.Println("Test solution 2 =", sol2_1_test, "->", checkSolution(sol2_1_test, testSolution2))
+	fmt.Println()
+	fmt.Println("=== Part 2 ===")
+	sol2_1_test := solve2(rollsMapTest, rollsPosTest, rollsChar, true)
+	fmt.Println("Test solution 2 =", sol2_1_test, "->", checkSolution(sol2_1_test, testSolution2))
 
-	// t1 = time.Now()
-	// sol2 := solve2(batteryBanks1, false)
-	// dur = time.Since(t1)
-	// fmt.Println("Solution part 2 =", sol2, "(ET =", dur, ")")
+	t1 = time.Now()
+	sol2 := solve2(rollsMap1, rollsPos1, rollsChar, false)
+	dur = time.Since(t1)
+	fmt.Println("Solution part 2 =", sol2, "(ET =", dur, ")")
 }
