5869d3f7026a74d00da5709f61bd36a6795a0a95
svagelj <vito.svagelj@gmail.com>
2025-12-07 19:27:01 +0100

    adds 2025 day 7 - part 1

---
+++ 2025/day_07/day_07.go
@@ -0,0 +1,183 @@
+package main
+
+import (
+	// Math "aoc_2025/tools/Math"
+	Array "aoc_2025/tools/Array"
+	Printer "aoc_2025/tools/Printer"
+	rw "aoc_2025/tools/rw"
+	"time"
+
+	"fmt"
+	"strconv"
+
+	// "slices"
+	// "sort"
+	// "strings"
+	// "bufio"
+	// "os"
+)
+
+// var must be used for global variables
+var testData = []string {
+	".......S.......",
+	"...............",
+	".......^.......",
+	"...............",
+	"......^.^......",
+	"...............",
+	".....^.^.^.....",
+	"...............",
+	"....^.^...^....",
+	"...............",
+	"...^.^...^.^...",
+	"...............",
+	"..^...^.....^..",
+	"...............",
+	".^.^.^.^.^...^.",
+	"...............",
+}
+
+var testSolution1, testSolution2 = 21, -1
+
+//------------------------------------------------------
+
+func checkSolution(testValue int, solValue int) string {
+	if testValue == solValue {
+		return "passed   :)"
+	} else {
+		return "failed   :(   (should be '"+strconv.Itoa(solValue)+"')"
+	}
+}
+
+//------------------------------------------------------
+
+func initData(fileLines []string) ([][]rune, []int) {
+
+	diagram := [][]rune {}
+	start := []int {}
+
+	startChar := 'S'
+
+	for i := range fileLines {
+		line := fileLines[i]
+
+		_line := []rune {}
+		for j := range line {
+			_line = append(_line, rune(line[j]))
+			if rune(line[j]) == startChar {
+				start = []int {i,j}
+			}
+		}
+		diagram = append(diagram, _line)
+	}
+
+	return diagram, start
+}
+
+func solve1(diagram [][]rune, start []int, printout bool) int {
+
+	if printout {
+		Printer.PrintGridRune(diagram, 1)
+		fmt.Println("start", start)
+	}
+
+	queue := [][]int {start}
+	splitterChar := '^'
+	laserPath := Array.CopyRune2D(diagram)
+	visitedSplitters := Array.InitArrayValuesInt(len(diagram), len(diagram[0]), 0)
+	sizeY := len(diagram)
+
+	for k := 0; k < len(queue); k++ {
+		// the loop must include len(queue) because it is expanding
+
+		newQueue := [][]int {}
+		y := queue[k][0]
+		x := queue[k][1]
+
+		// current place was already visited
+		if laserPath[y][x] == '|' {
+			continue
+		}
+
+		laserPath[y][x] = '|'
+
+		for j := y+1; j < sizeY; j++ {
+			if diagram[j][x] == splitterChar {
+				newQueue = append(newQueue, []int{j, x-1})
+				newQueue = append(newQueue, []int{j, x+1})
+				
+				visitedSplitters[j][x] = visitedSplitters[j][x] + 1
+				break
+			}
+
+			laserPath[j][x] = '|'
+		}
+
+		queue = append(queue, newQueue...)
+	}
+
+	if printout {
+		fmt.Println("Finished")
+		Printer.PrintGridRune(laserPath, 1)
+		fmt.Println()
+		Printer.PrintGridInt(visitedSplitters, 2)
+	}
+	
+	// Count visited splitters
+	sum := 0
+	for i := range visitedSplitters {
+		for j := range visitedSplitters[i] {
+			if visitedSplitters[i][j] != 0 {
+				sum = sum + 1
+			}
+		}
+	}
+
+	return sum
+}
+
+//----------------------------------------
+
+func solve2(data [][]string, printout bool) int {
+
+	if printout {
+		fmt.Println(data)
+	}
+
+	sum := 0
+	return sum
+}
+
+func main() {
+
+	// data gathering and parsing
+	diagramTest1, startTest1 := initData(testData)
+
+	fileName := "day_07_data.txt"
+	fileData := rw.ReadFile(fileName)
+	diagram1, start1 := initData(fileData)
+
+	// ---------------------------------------------
+	fmt.Println("=== Part 1 ===")
+	sol1_1_test := solve1(diagramTest1, startTest1, true)
+	fmt.Println("Test solution 1 =", sol1_1_test, "->", checkSolution(sol1_1_test, testSolution1))
+
+	// 1775 is too high
+
+	t1 := time.Now()
+	sol1 := solve1(diagram1, start1, false)
+	dur := time.Since(t1)
+	fmt.Println("Solution part 1 =", sol1, "(ET =", dur, ")")
+
+	// ---------------------------------------------
+
+	// fmt.Println()
+	// fmt.Println("=== Part 2 ===")
+	// sol2_1_test := solve2(dataTest2, true)
+	// fmt.Println("Test solution 2 =", sol2_1_test, "->", checkSolution(sol2_1_test, testSolution2))
+
+	// t1 = time.Now()
+	// sol2 := solve2(data2, false)
+	// dur = time.Since(t1)
+	// fmt.Println("Solution part 2 =", sol2, "(ET =", dur, ")")
+}
